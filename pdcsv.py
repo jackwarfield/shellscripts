af5fede45ff40f90775c2e812a99453cbc430646
Jack Warfield <jtw5zc@virginia.edu>
2022-05-10 14:23:27 -0400

    also import numpy

--- pdcsv.py
+++ pdcsv.py
@@ -1,7 +1,8 @@
 #! /usr/local/Caskroom/miniconda/base/envs/astro/bin/python3.9
 # For opening an ipython session with the target table loaded in pandas
-
 import sys
+
+import numpy as np
 import pandas as pd
 
 df = pd.read_csv(sys.argv[1], low_memory=False)
